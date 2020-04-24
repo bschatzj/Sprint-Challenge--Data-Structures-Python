8c550753b2e8c87924505fbab0ad175c6ec5d9a5
Brendan <brendanschatz97@gmail.com>
2020-04-24 11:57:51 -0400

    Names done!

---
+++ names/binary_search_tree.py
@@ -0,0 +1,30 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value and self.left is None:
+            self.left = BinarySearchTree(value)
+            return
+        elif value >= self.value and self.right is None:
+            self.right = BinarySearchTree(value)
+            return
+        if value < self.value:
+            self.left.insert(value)
+        else:
+            self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        else:
+            if target < self.value and self.left:
+                return self.left.contains(target)
+            elif target > self.value and self.right:
+                return self.right.contains(target)
+        return False
